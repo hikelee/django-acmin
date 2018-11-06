c6e22b9c9eb939018187fe09ff9ed138682ff39e
hikelee <hikelee@gmail.com>
2018-11-06 12:33:41 +0800

    0.1.6

--- acmin/processors.py
+++ acmin/processors.py
@@ -2,6 +2,6 @@ from django.conf import settings
 
 def extra_context(request):
     return {
-        'app_name': getattr(settings, "APP_NAME"),
-        "function_name": getattr(settings, "FUNCTION_NAME"),
+        'app_name': getattr(settings, "ACMIN_APP_NAME"),
+        "function_name": getattr(settings, "ACMIN_FUNCTION_NAME"),
     }
