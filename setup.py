f6b32b3c208137e6c8a3f272866b39dbb1925e2a
hikelee <hikelee@gmail.com>
2018-11-27 12:10:49 +0800

    0.1.25

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import os
 
 from setuptools import find_packages, setup
 
-version = "0.1.24"
+version = "0.1.25"
 
 with open(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'requirements.txt')) as f:
     required = f.read().splitlines()
