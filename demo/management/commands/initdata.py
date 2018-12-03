be4f15b902bf31d447d1a15b065f1e14f6ac3e51
hikelee <hikelee@gmail.com>
2018-12-03 10:34:58 +0800

    update

--- demo/management/commands/initdata.py
+++ demo/management/commands/initdata.py
@@ -1,29 +1,40 @@
-import time
-
-import django.apps
-import requests
 from django.core.management.base import BaseCommand
-from demo.models import Province, City, Area, Address, Author, Book, Member, User, Order
+
+from acmin.models import User, Group
+from demo.models import Province, City, Area, Address, Author, Book, Member, Order
 
 
 class Command(BaseCommand):
     """
         python manage.py initdata
     """
+
     def handle(self, *args, **options):
-        for model in [Province, City, Area, Address, Author, Book, Member, User, Order]:
+
+        for model in [Province, City, Area, Address, Author, Book, Member, Order]:
             model.objects.all().delete()
+
+        group = Group.objects.update_or_create(name="员工")[0]
+
+        def create_user(name):
+            user = User.objects.filter(username=name).first()
+            if not user:
+                user = User(group=group, username=name, title=name)
+                user.set_password("123456")
+                user.save()
+            return user
+
+        user1 = create_user("user1")
+        user2 = create_user("user2")
+
         province = Province.objects.create(name="广东省")
         city = City.objects.create(province=province, name="深圳市")
         area = Area.objects.create(city=city, name="南山区")
         address = Address.objects.create(area=area, detail="科技园")
         author = Author.objects.create(address=address, name="令将")
         book = Book.objects.create(author=author, name="陈天故事", price=12)
-        member = Member.objects.create(name="M1")
-        user = User.objects.create_superuser(
-            'admin', 'emailname@demon.com', '123456')
-        user.title = "admin"
-        user.save()
-        Order.objects.create(follower=user, address=address,
-                             member=member, book=book, amount=2)
+        member1 = Member.objects.create(name="M1")
+        member2 = Member.objects.create(name="M2")
+        Order.objects.create(follower=user1, address=address, member=member1, book=book, amount=2)
+        Order.objects.create(follower=user2, address=address, member=member2, book=book, amount=2)
         print("ok")
