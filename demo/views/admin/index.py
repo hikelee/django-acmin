1e0f9478e890bc3840db72f47c30023e22ac85a8
hikelee <hikelee@gmail.com>
2018-12-07 10:38:05 +0800

    update

--- demo/views/admin/index.py
+++ demo/views/admin/index.py
@@ -10,27 +10,35 @@ from acmin.utils import attr, import_model
 
 
 def get_nodes(user):
+    default_node = ""
     nodes = [
         ("基础信息", [import_model("acmin", ct.name) for ct in ContentType.objects.filter(app="acmin")]),
         ("业务信息", [import_model("demo", ct.name) for ct in ContentType.objects.filter(app="demo")]),
     ]
     result = []
-    for name, models in nodes:
-        sub = [(model.__name__, attr(model, "_meta.verbose_name")) for model in models if
-               Permission.has_permission(user, model, PermissionItem.listable)]
-        if sub:
-            result.append((name, sub))
-    return result
+    for title, models in nodes:
+        leaves = []
+        for model in models:
+            if Permission.has_permission(user, model, PermissionItem.listable):
+                class_name = model.__name__
+                leaves.append((class_name, attr(model, "_meta.verbose_name")))
+                if not default_node:
+                    default_node = class_name
+        if leaves:
+            result.append((title, leaves))
+    return result, default_node
 
 
 @login_required
 def index(request):
+    nodes, default_node = get_nodes(request.user)
     context = {
         "random": ''.join(random.sample(string.ascii_letters + string.digits, 8)),
-        "nodes": get_nodes(request.user),
+        "nodes": nodes,
         "site_name": "acmin demo",
         "copyright": "acmin © 2016-2018",
         "role_name": "",
+        "default_node": default_node
 
     }
     return HttpResponse(loader.get_template('admin/main.html').render(context, request))
