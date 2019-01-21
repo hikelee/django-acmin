4e35e528fe8e00a11f7d678f0c57bc45ba61b563
hikelee <hikelee@gmail.com>
2019-01-21 08:56:47 +0800

    update

--- demo/views/api/auth.py
+++ demo/views/api/auth.py
@@ -11,6 +11,35 @@ def logout_view(request):
     return dict(status=200)
 
 
+@api_route("api/demo/auth/userInfo/")
+def user_info(request):
+    user = request.user
+    if user is not None:
+        return {
+            "status": 200,
+            "data": {
+                "name": "ROOT",
+                "avatar": "https://gw.alipayobjects.com/zos/rmsportal/BiazfanxmamNRoxxVxka.png",
+                "unreadCount": 11,
+                "email": "antdesign@alipay.com",
+                "signature": "海纳百川，有容乃大",
+                "title": "交互专家",
+                "group": "蚂蚁金服－某某某事业群－某某平台部－某某技术部－UED",
+                "tags": [
+                    {"key": "0", "label": "很有想法的"},
+                    {"key": "1", "label": "专注设计"},
+                    {"key": "2", "label": "辣~"},
+                    {"key": "3", "label": "大长腿"},
+                    {"key": "4", "label": "川妹子"},
+                    {"key": "5", "label": "海纳百川"}
+                ]
+            },
+            "message": "success"
+        }
+    else:
+        return dict(status=403, message="login failed")
+
+
 @api_route("api/demo/auth/login/", login_required=False)
 def login_view(request):
     obj = json.loads(request.body)
@@ -19,11 +48,6 @@ def login_view(request):
     user = authenticate(request, username=username, password=password)
     if user is not None:
         login(request, user)
-        return dict(
-            status=200,
-            data=dict(
-                title=user.title,
-                avatar="https://gw.alipayobjects.com/zos/rmsportal/BiazfanxmamNRoxxVxka.png",
-            ))
+        return dict(status=200)
     else:
         return dict(status=403, message="login failed")
